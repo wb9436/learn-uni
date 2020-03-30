9110b4b51cb9fb95bf82b68329f89f0bb8241fe4
wb9436 <1016065991@qq.com>
2020-03-30 16:18:25 +0800

    初始提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,38 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <image class="logo" v-bind:src="logoPng"></image>
+        <view>
+            <text class="title">{{title}}</text>
+        </view>
+        <view>
+            <text class="title">{{loadTime}}</text>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+    import LogoPng from '../../static/logo.png'
+    import * as DateUtil from '../../utils/dateUtil'
 
-		},
-		methods: {
+    export default {
+        data() {
+            return {
+                logoPng: LogoPng,
+                title: 'Hello'
+            }
+        },
+        onLoad() {
 
-		}
-	}
+        },
+        computed: {
+            loadTime: () => {
+                return DateUtil.dateFormat(new Date())
+            }
+        },
+        methods: {}
+    }
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+<style scoped lang="scss">
+    @import "./index";
 </style>
